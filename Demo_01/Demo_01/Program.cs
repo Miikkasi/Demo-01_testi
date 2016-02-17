3db6adbe06341342300e875e6cd426800cd67151
Miikka Sipilä <k3051@student.jamk.fi>
2016-02-17 09:08:12 +0200

    Eka

---
+++ Demo_01/Demo_01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // määritellään muuttuja
+            string nimi = "Matti Mainio";
+            // tulostetaan muuttujan arvo konsolille
+            Console.WriteLine("Nimi on " + nimi);
+            // pidetään sovelluksen ajoikkuna näytöllä. Sulkeutuu, kun enter-painiketta on painettu
+            Console.ReadLine();
+        }
+    }
+}
